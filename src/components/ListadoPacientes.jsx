156792cacfb93beb23335b3602b087884c2bc01b
molinak97 <molinavillanuevak@gmail.com>
2024-03-13 00:35:19 -0600

    Funciona pero tienes un arreglo dentro de otro arreglo

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Paciente from './Paciente'
 
-export default function ListadoPacientes() {
+const ListadoPacientes = ({pacientes}) => {
+
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
       <h2 className=' font-black text-3xl text-center'>Listado Pacientes</h2>
@@ -9,10 +10,16 @@ export default function ListadoPacientes() {
         Administra tus {''}
         <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
       </p>
-      <Paciente/>
-      <Paciente/>
-      <Paciente/>
+
+      {pacientes.map( paciente => (
+          <Paciente
+            paciente={paciente}
+          />
+        ))}
+      
+
     </div>
   )
 }
 
+export default ListadoPacientes
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,8 +1,8 @@
-const Paciente = () => {
+const Paciente = (paciente) => {
   return (
     <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
         <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre:{''}
-        <span className='font-normal normal-case'>Hook</span>
+        <span className='font-normal normal-case'>{paciente.paciente.nombre}</span>
         </p>
 
         <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario:{''}
